f3914aaac2713fe1f264c153175f3d1ef193b88e
Niklas Fulle <36989748+Salkin263@users.noreply.github.com>
2023-09-25 23:24:32 +0200

    added function for the contact form

--- src/app/api/email/send/route.ts
+++ src/app/api/email/send/route.ts
@@ -1,13 +1,15 @@
+import { sendContactMail } from "@/lib/helpers/send-mail";
 import { NextResponse } from "next/server";
 
 export async function POST(
   req: Request
 ) {
   try {
-
     const body: any = await req.json();
 
-    return NextResponse.json({ message: "Project created succsesful", success: true }, { status: 200 })
+    await sendContactMail(body.contactEmail, body.senderEmail, body.topic, body.message);
+
+    return NextResponse.json({ message: "Message was send succsesful", success: true }, { status: 200 })
   } catch (error) {
     console.log(error)
     return NextResponse.json({ message: error, success: false }, { status: 400 })
--- src/components/main/Contact.tsx
+++ src/components/main/Contact.tsx
@@ -1,18 +1,61 @@
 "use client";
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 import SectionHeading from "@/components/SectionHeading";
 import { Input } from "@/ui/Input";
 import { Textarea } from "@/ui/Textarea";
 import { Button } from "@/ui/Button";
+import { shortToast } from "@/lib/helpers/shorter-function";
 
 interface Props {
   contactEmail: string;
 }
 
 const Contact = ({ contactEmail }: Props) => {
+  const [sendingEmail, setSendingEmail] = useState(false);
   const { ref } = useSectionInView("Contact");
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setSendingEmail(true);
+
+    try {
+      const formData = new FormData(e.currentTarget);
+
+      const data = {
+        contactEmail,
+        senderEmail: formData.get("senderEmail"),
+        topic: formData.get("topic"),
+        message: formData.get("message"),
+      };
+
+      const res = await fetch("/api/email/send", {
+        method: "POST",
+        body: JSON.stringify(data),
+      });
+
+      if (res.status === 200) {
+        shortToast(
+          "Success",
+          "The message was sent successfully.",
+          "success",
+          5000
+        );
+      }
+
+      e.target.reset();
+    } catch (error) {
+      shortToast(
+        "Error",
+        "There was an error sending the message.",
+        "error",
+        5000
+      );
+    }
+
+    setSendingEmail(false);
+  };
 
   return (
     <motion.section
@@ -42,7 +85,10 @@ const Contact = ({ contactEmail }: Props) => {
         or through this form.
       </p>
 
-      <form className="mt-10 flex flex-col items-center dark:text-black">
+      <form
+        onSubmit={handleSubmit}
+        className="mt-10 flex flex-col items-center dark:text-black"
+      >
         <Input
           className="mt-4 h-14 rounded-lg border border-gray-700 px-4 shadow-md transition-all placeholder:text-gray-900 focus:ring-[#5bb0ff] dark:bg-white dark:outline-none dark:focus:ring-[#ff9a60]"
           name="senderEmail"
@@ -53,8 +99,8 @@ const Contact = ({ contactEmail }: Props) => {
         />
         <Input
           className="mt-4 h-14 rounded-lg border border-gray-700 px-4 shadow-md transition-all placeholder:text-gray-900 focus:ring-[#5bb0ff] dark:bg-white dark:outline-none dark:focus:ring-[#ff9a60]"
-          name="senderEmail"
-          type="email"
+          name="topic"
+          type="text"
           required
           maxLength={500}
           placeholder="Your Topic"
@@ -66,7 +112,11 @@ const Contact = ({ contactEmail }: Props) => {
           required
           maxLength={5000}
         />
-        <Button className="mt-6 w-2/6 bg-[#5bb0ff] font-semibold text-gray-900 shadow-md hover:bg-[#4a8dcc] hover:text-gray-100 dark:bg-[#ff9a60] dark:text-white dark:hover:bg-[#fc8c4bd0]">
+        <Button
+          isLoading={sendingEmail}
+          disabled={sendingEmail}
+          className="mt-6 w-2/6 bg-[#5bb0ff] font-semibold text-gray-900 shadow-md hover:bg-[#4a8dcc] hover:text-gray-100 dark:bg-[#ff9a60] dark:text-white dark:hover:bg-[#fc8c4bd0]"
+        >
           Send
         </Button>
       </form>
